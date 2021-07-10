d9b510ca9a13c90ba5d2604dc6de375ac2f773ee
Austin W. Milne <awbmilne@gmail.com>
2021-07-09 20:39:18 -0400

    Major updates and fixes
    - Fix unit conversions for caluculation
    - Remove axis scales for graphics
    - Add frame divider and reduce frame output (MUCH faster)
    - Add LaTeX report file
    - Add VSCode tasks for running matlab, optimizing video, and
    generating report.
    - Reorganize file structure for better navigation and readability
    - Add .gitignore and .gitkeep files to maintain structure
    
    Signed-off-by: Austin W. Milne <awbmilne@gmail.com>

---
+++ BilliardsCode.m
@@ -0,0 +1,156 @@
+%% Setup Environment
+% Clear the commands and variables
+close all;
+clear all;
+clc;
+output_dir = "out/matlab/";
+% Start logging the output to diary file
+dfile = output_dir + 'output.log';
+if exist(dfile, 'file') ; delete(dfile); end
+diary(dfile)
+diary on;
+
+%% Setup Simulation parameters
+% Set the time interval for calculations
+% (Determines precision)
+time_slice = 0.000001; % Recompute position and velocity every 1/1,000,000 of a second
+slices_per_sec = 1 / time_slice; % Theoretical frames/sec
+frame_divider = round(slices_per_sec / 60); % Aim for roughly 60 fps for output video
+frame_rate = slices_per_sec/frame_divider;
+
+%% Setup Frame and Video
+% Create frame and video objects for storing video frames
+latest_frame = 0;
+writerObj = VideoWriter(output_dir + 'videos/elasticCollision.avi');
+writerObj.FrameRate = frame_rate;
+writerObj.Quality = 95;
+open(writerObj);
+
+% Background
+patch([ 0, 0,88,88],[ 0,44,44, 0], 'g','FaceAlpha',0.5); 
+grid on;
+axis equal;
+axis off;
+axis([0 PoolBall.table_length 0 PoolBall.table_width])
+hold on;
+
+%% Setup Pool Balls
+% Create array of Pool Balls with given properties and locations
+%                 | Mass(g) | Rad(in) | Position(in) |    Colour(RGB)   | TT | is_cue?
+balls = [PoolBall(       200,     2.25, [66.00,22.00], [255,255,255]/255, 'C',   true), ...
+         PoolBall(       160,     2.25, [40.08,21.23], [255,255,  0]/255, '1',  false), ...
+         PoolBall(       160,     2.25, [17.52,37.29], [  0,  0,255]/255, '2',  false), ...
+         PoolBall(       160,     2.25, [12.53, 2.75], [255,  0,  0]/255, '3',  false), ...
+         PoolBall(       160,     2.25, [10.47,25.10], [144,  0,255]/255, '4',  false), ...
+         PoolBall(       160,     2.25, [27.32, 4.87], [255,144,  0]/255, '5',  false), ...
+         PoolBall(       160,     2.25, [16.25,13.80], [  0,255,  0]/255, '6',  false), ...
+         PoolBall(       160,     2.25, [ 7.60,21.49], [163,166, 27]/255, '7',  false), ...
+         PoolBall(       160,     2.25, [10.81, 9.21], [  0,  0,  0]/255, '8',  false), ...
+         PoolBall(       160,     2.25, [26.19,21.84], [138, 92, 51]/255, '9',  false), ...
+         PoolBall(       160,     2.25, [19.78,26.81], [  0,  0,144]/255, '10', false)];
+cue_ball = balls(1);
+
+% Determine Closest ball to cue and angle for shot
+closest_ball = balls(1).find_nearest(balls(2:end));
+vector_between = closest_ball.pos - balls(1).pos;
+theta_closest = atan2(vector_between(2), vector_between(1));
+fprintf("Closest ball to Cue ball: Ball %2s\n", closest_ball.label)
+fprintf("Attitude to closest ball: %7.3f deg\n", rad2deg(theta_closest));
+fprintf("\n");
+
+% Create the array of Cue Ball Velocities to test
+cue_velocities = [
+                  [ -cos(deg2rad( 0))*  4,   sin(deg2rad( 0))*  4];... % 4 m/s directly left
+                  [ -cos(deg2rad(15))*2.5,   sin(deg2rad(15))*2.5];... % 2.5 m/s @ 15 deg upward toward the left
+                  [ -cos(deg2rad(30))*1.5,   sin(deg2rad(30))*1.5];... % 1.5 m/s @ 30 deg upward toward the left
+                  [cos(theta_closest)*1.5, sin(theta_closest)*1.5];... % 1.5 m/s @ angle to closest ball
+                 ];
+
+
+%% Physics Simulation
+% Interate over each cue velocity to test
+for i=1: length(cue_velocities)
+    % Reset the pool balls
+    for j=1: length(balls)
+        balls(j).reset;
+    end
+
+    % Save the start position as an image
+    exportgraphics(gca,output_dir + sprintf("images/Simulation_%i_start.png", i),'Resolution',600)
+
+    % Set the cue velocity for the test
+    cue_ball.set_vel(cue_velocities(i,:));
+
+    frame_counter=0;
+    % Simulate the shot
+    while(true)
+        % Check if any balls are still moving.
+        any_moving=false;
+        for j=1:length(balls)
+            if (any(balls(j).vel))
+                any_moving=true;
+                break;
+            end
+        end
+        % If none are moving, stop calculating frames.
+        if (not(any_moving))
+            % Render the last frame, regardless of divider
+            for j=1:length(balls)
+                balls(j).draw(); % Draw the current position of the ball on the graph
+            end
+            drawnow; % Render the graph
+            latest_frame = getframe(gcf); % Save the rendered graph as a video frame
+            break;
+        end
+
+        % Increment frame counter
+        frame_counter = frame_counter + 1;
+        
+        % Calculate motion for each ball
+        for j=1:length(balls)
+            balls(j).move(time_slice); % Update the position with velocity and apply rolling friction
+            balls(j).compute_wall_collisions(); % Compute the velocity changes from wall collisions
+            % Collide each ball with all the balls after it in the array
+            % (Covers all combinations)
+            if j+1 <= length(balls)
+                for k=j+1:length(balls)
+                    balls(j).compute_ball_collision(balls(k)); % Compute collision of balls
+                end
+            end
+        end
+        
+        % Write frames to the video, subject to the frame divider
+        if (frame_counter == 0 || mod(frame_counter, frame_divider) == 0)
+            for j=1:length(balls)
+                balls(j).draw(); % Draw the current position of the ball on the graph
+            end
+            drawnow; % Render the graph
+            latest_frame = getframe(gcf); % Save the rendered graph as a video frame
+            writeVideo(writerObj, latest_frame); % Write the frame to the overall video file
+        end
+    end
+
+    % Output the simulation results
+    fprintf("Simulation %i: Cue velocity = [%6.3f,%6.3f] (m/s)\n", i, cue_velocities(i,1), cue_velocities(i,2));
+    fprintf("    Final Ball positions:\n")
+    for j=1: length(balls) % List all ball positions
+        fprintf("    Ball %2s: [%7.4f, %7.4f] (in)\n", ...
+                balls(j).label,                        ...
+                convlength(balls(j).pos(1),'m','in'),  ...
+                convlength(balls(j).pos(2),'m','in')); ...
+    end
+    fprintf("\n");
+
+    % Save the final position of the balls as an image
+    exportgraphics(gca,output_dir + sprintf("images/Simulation_%i_end.png", i),'Resolution',600)
+
+    % Add the latest frame to the video repeatedly for the next 1 second
+    % (Creates a 1 second pause between video sections)
+    for j=1: frame_rate
+        writeVideo(writerObj, latest_frame)
+    end
+end
+
+%% Clean up Script
+close(writerObj); % Wrap up the video file
+diary off; % Close the diary file
--- PoolBall.m
+++ PoolBall.m
@@ -1,38 +1,46 @@
+%% PoolBall class
+% Carries the properties and states of a ball on the table
 classdef PoolBall < handle
+    %% Shared Properties
     properties (Constant)
-        table_length = 88;
-        table_width = 44;
+        table_length = convlength(88,'in','m');  % Length of pool table (x-direction)
+        table_width =  convlength(44,'in','m'); % Width of pool table (y-direction)
 
-        mu_bb = 0.05;
-        mu_bs = 0.10;
-        mu_bc = 0.20;
+        mu_bb = 0.05; % Coefficient of friction between 2 balls
+        mu_bs = 0.10; % Coefficient of friction between ball and surface
+        mu_bc = 0.20; % Coefficient of friction between ball and wall cushion/bumper
 
-        e_cn = 0.95;
-        e_nn = 0.90;
-        e_bc = 0.70;
+        e_cn = 0.95; % Coefficient of resistution between cue ball and numbered ball
+        e_nn = 0.90; % Coefficient of restitution between 2 numbered balls
+        e_bc = 0.70; % Coefficient of restitution between ball and wall cushion/bumper
+        
+        gravity = 9.80665; % Earths gravity in m/s
     end
+    %% Particular Properties
     properties
-        mass;
-        rad;
-        home_pos
-        pos;
-        home_vel
-        vel;
-        color;
-        label;
-        is_cue;
-        colliding_balls= PoolBall.empty;
-        ball_img;
-        spot_img;
-        label_img;
+        mass; % Mass of the ball (kg)
+        rad;  % Radius of the ball (m)
+        home_pos % Starting position of the ball (m)
+        pos; % Current position of the ball (m)
+        vel; % Current velocity of the ball (m/s)
+        color; % Color of the ball (RGB matrix)
+        label; % Label of the ball (1-2 Characters)
+        is_cue; % Boolean specifying if this ball is a/the cue ball
+        colliding_balls= PoolBall.empty; % List of balls currently in collision with this
+        ball_img; % Image object for the ball
+        spot_img; % White spot object for the ball
+        label_img; % Label img for the ball
     end
+    %% Methods
     methods
+        %% PoolBall Constructor
+        % Initializes the ball given the specified parameters
         function PB = PoolBall(mass,rad,pos,color,label,is_cue)
-            PB.mass = mass;
-            PB.rad = rad;
-            PB.home_pos = pos;
-            PB.pos = pos;
-            PB.vel = [0,0];
+            PB.mass = mass / 1000; % Convert grams to kilograms
+            PB.rad = convlength(rad, 'in', 'm'); % Convert inches to meters
+            PB.home_pos = convlength(pos, 'in', 'm'); % Convert inches to meters
+            PB.pos = PB.home_pos;
+            PB.vel = [0,0]; % Keep m/s as m/s
             PB.color = color;
             PB.label = label;
             PB.is_cue = is_cue;
@@ -43,19 +51,22 @@ classdef PoolBall < handle
             PB.spot_img = patch (PB.pos(1)+PB.rad*Xcircle/2, PB.pos(2)+PB.rad*Ycircle/2, 'w', 'FaceAlpha', 1, 'LineStyle' ,'none');
             PB.label_img = text(PB.pos(1), PB.pos(2), PB.label, 'FontSize', 7, 'Color', 'b', 'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle', 'FontWeight', 'bold');
         end
+        %% PoolBall Move
+        % Update the position and velocity of the pool ball based in the
+        % velocity and surface friction
         function move(obj, time_slice)
-            gravity=9.80665;
             vel_angle = atan2(obj.vel(2), obj.vel(1));
             old_vel = norm(obj.vel); % Get the normal of the old velocity
-            new_vel = old_vel - ((time_slice * PoolBall.mu_bs * gravity)); % Loss of velocity from friction
+            new_vel = old_vel - (time_slice * PoolBall.mu_bs * PoolBall.gravity); % Loss of velocity from friction
             if (new_vel < 0) 
                 new_vel = 0; % Check if the new velocity was reduced past 0
             end
-            effective_vel = (old_vel + new_vel)/2;
-            effective_vel = [cos(vel_angle) * effective_vel, sin(vel_angle) * effective_vel];
+            effective_vel = [cos(vel_angle) * (old_vel + new_vel)/2, sin(vel_angle) * (old_vel + new_vel)/2];
             obj.vel = [cos(vel_angle) * new_vel, sin(vel_angle) * new_vel];
             obj.pos = obj.pos + effective_vel * time_slice; % Update the objects position
         end
+        %% PoolBall Wall Collisions
+        % Compute velocity changes due to wall/cushion/bumper collisions
         function compute_wall_collisions(obj)
             % Check if we collided with the x=max or x=0 "bumpers"
             max_x = PoolBall.table_length;
@@ -80,10 +91,16 @@ classdef PoolBall < handle
                 end
             end
         end
+        %% PoolBall Ball Collisions
+        % Compute velocity cahnges to ball<->ball collisions
         function compute_ball_collision(ball_a, ball_b)
             % Check if the balls collided
             if (norm(ball_a.pos - ball_b.pos) < (ball_a.rad + ball_b.rad))
                 % Check if we are still colliding with this ball
+                %   Approximation method needed for time-slice based approach.
+                %   This avoids calculating multiple collisions while balls are
+                %   clipped through eachother. Decreased time-slice reduces approximation
+                %   error here.
                 if (~isempty(ball_a.colliding_balls))
                     for i=1: length(ball_a.colliding_balls)
                         if (isequal(ball_b, ball_a.colliding_balls(i)))
@@ -102,22 +119,22 @@ classdef PoolBall < handle
                 % Get the angles velocities, relative to the impact
                 difference = ball_b.pos - ball_a.pos;
                 impact_angle = atan2(difference(2), difference(1));
-                ball_a_v = norm(ball_a.vel);
-                ball_a_v_angle = atan2(ball_a.vel(2), ball_a.vel(1));
-                ball_b_v = norm(ball_b.vel);
-                ball_b_v_angle = atan2(ball_b.vel(2), ball_b.vel(1));
-                ball_a_norm_v = cos(ball_a_v_angle - impact_angle) * ball_a_v;
-                ball_a_tan_v = sin(ball_a_v_angle - impact_angle) * ball_a_v;
-                ball_b_norm_v = cos(ball_b_v_angle - impact_angle) * ball_b_v;
-                ball_b_tan_v = sin(ball_b_v_angle - impact_angle) * ball_b_v;
+                ball_a_v = norm(ball_a.vel); % Absolute velocity of A
+                ball_a_v_angle = atan2(ball_a.vel(2), ball_a.vel(1)); % Angle of velocity of A
+                ball_b_v = norm(ball_b.vel); % Absolute velocity of B
+                ball_b_v_angle = atan2(ball_b.vel(2), ball_b.vel(1)); % Angle of velocity of B
+                ball_a_norm_v = cos(ball_a_v_angle - impact_angle) * ball_a_v; % norm portion of A velocity (compared to impact direction)
+                ball_a_tan_v = sin(ball_a_v_angle - impact_angle) * ball_a_v;  % tan portion of A velocity (compared to impact direction)
+                ball_b_norm_v = cos(ball_b_v_angle - impact_angle) * ball_b_v; % norm portion of B velocity (compared to impact direction)
+                ball_b_tan_v = sin(ball_b_v_angle - impact_angle) * ball_b_v;  % tan portion of B velocity (compared to impact direction)
                 % Create system of equations for the normal velocities, and solve
                 syms new_ball_a_norm_v new_ball_b_norm_v
                 norm_momentum = ball_a_norm_v * ball_a.mass + ball_b_norm_v * ball_b.mass == new_ball_a_norm_v * ball_a.mass + new_ball_b_norm_v * ball_b.mass;
                 norm_restitution = e == (new_ball_a_norm_v - new_ball_b_norm_v)/(ball_b_norm_v - ball_a_norm_v);
                 [A,B] = equationsToMatrix([norm_momentum, norm_restitution], [new_ball_a_norm_v, new_ball_b_norm_v]);
                 X = linsolve(A,B);
-                new_ball_a_norm_v = X(1);
-                new_ball_b_norm_v = X(2);
+                new_ball_a_norm_v = X(1); % Post-impact normal velocity of A
+                new_ball_b_norm_v = X(2); % Post-impact normal velocity of B
                 % Determine frictional losses on tangent velocities
                 new_ball_a_tan_v = ball_a_tan_v - sign(ball_a_tan_v)*(PoolBall.mu_bb * (ball_a_norm_v - new_ball_a_norm_v));
                 new_ball_b_tan_v = ball_b_tan_v - sign(ball_b_tan_v)*(PoolBall.mu_bb * (ball_b_norm_v - new_ball_b_norm_v));
@@ -129,12 +146,12 @@ classdef PoolBall < handle
                     new_ball_b_tan_v = 0;
                 end
                 % Apply new velocities to balls
-                ball_a.vel = [double(cos(impact_angle)*new_ball_a_norm_v - cos(pi/2 - impact_angle)*new_ball_a_tan_v),
+                ball_a.vel = [double(cos(impact_angle)*new_ball_a_norm_v - cos(pi/2 - impact_angle)*new_ball_a_tan_v), ...
                               double(sin(impact_angle)*new_ball_a_norm_v + sin(pi/2 - impact_angle)*new_ball_a_tan_v)];
-                ball_b.vel = [double(cos(impact_angle)*new_ball_b_norm_v - cos(pi/2 - impact_angle)*new_ball_b_tan_v),
+                ball_b.vel = [double(cos(impact_angle)*new_ball_b_norm_v - cos(pi/2 - impact_angle)*new_ball_b_tan_v), ...
                               double(sin(impact_angle)*new_ball_b_norm_v + sin(pi/2 - impact_angle)*new_ball_b_tan_v)];
                 return;
-            % Check if we stopped colliding with this ball
+            % Check if we finished colliding with this ball
             elseif (~isempty(ball_a.colliding_balls))
                 for i=1: length(ball_a.colliding_balls)
                     if (isequal(ball_b, ball_a.colliding_balls(i)))
@@ -143,6 +160,8 @@ classdef PoolBall < handle
                 end
             end
         end
+        %% PoolBall Draw
+        % Draw the ball on the graph at the current position
         function draw(obj)
             a=[0:0.1:2*pi];
             Xcircle=cos(a);
@@ -151,13 +170,20 @@ classdef PoolBall < handle
             set(obj.spot_img,'XData',obj.pos(1)+obj.rad*Xcircle/2, 'YData', obj.pos(2)+obj.rad*Ycircle/2);
             set(obj.label_img,'Position', [obj.pos(1) obj.pos(2) 0]);
         end
+        %% PoolBall Reset
+        % Reset the position and velocity of the ball
         function reset(obj)
             obj.pos = obj.home_pos;
             obj.vel = [0,0];
+            obj.draw();
         end
+        %% PoolBall Set Velocity
+        % Set the velocity of the pool ball (Clear than value accessor)
         function set_vel(obj, vel)
             obj.vel = vel;
         end
+        %% PoolBall Find Closest
+        % Determine the closest ball from a given list of ball
         function closest = find_nearest(obj, balls)
             closest = PoolBall.empty;
             min_dist = norm([PoolBall.table_length, PoolBall.table_width]);
